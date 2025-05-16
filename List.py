d32c932c204016e5f0a5737d754e070851c19c9d
SUHEL ANSARI <itsuhelansari@gmai.com>
2025-05-16 22:39:06 +0530

    Its my First Commit

---
+++ List.py
@@ -0,0 +1,13 @@
+'''list =[1,2,3,4,5,6,7,8,9,10]
+print(list)
+print(list[5])
+list.append(22)
+print(list)'''
+
+'''Fruits = ['Apple','Banana','Orange','mango','banana','pineapple']
+for i in range(len(Fruits) - 1, -1, -1):
+    print(Fruits[i], end=" ")'''
+x=5;
+for i in range(0,5-1):
+    print(i)
+
